8b6413efce30f797281e8ff997e239d8c59beb70
chaste-ganza <gchaste23@gmail.com>
2025-12-14 16:34:02 +0200

    git commit

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const right_eye = document.getElementById("right_eye");
 const left_eyelid = document.getElementById("left_eyelid");
 const right_eyelid = document.getElementById("right_eyelid");
 const mouth = document.getElementById("mouth");
+const upper_lip = document.getElementById("upper_lip");
 const grin = document.getElementById("grin");
 const sad = document.getElementById("sad");
 const surprise = document.getElementById("surprise");
 const smile = document.getElementById("smile");
+const happy = document.getElementById("happy");
+const wink = document.getElementById("wink");
+const frown = document.getElementById("frown");
+const neutral = document.getElementById("neutral");
 
 
 
@@ -25,9 +30,7 @@ function blink() {
 
   }, 3000);
 }
-
 blink();
-
 
 sad.addEventListener("click", ()=>{
   blink();
@@ -40,10 +43,10 @@ sad.addEventListener("click", ()=>{
     right_eyelid.style.height = "0%";
     left_eye.style.height = "70%";
     right_eye.style.height = "70%";
+    upper_lip.style.height = "0%";
 })
 
 grin.addEventListener("click", ()=>{
-  blink();
   mouth.style.borderTopRightRadius= "5px";
     mouth.style.borderTopLeftRadius= "5px";
     mouth.style.borderBottomLeftRadius= "50%";
@@ -53,16 +56,17 @@ grin.addEventListener("click", ()=>{
     right_eyelid.style.height = "0%";
     left_eye.style.height = "90%";
     right_eye.style.height = "90%";
+    upper_lip.style.height = "0%";
 })
 
 surprise.addEventListener("click", ()=>{
-  blink();
   mouth.style.borderRadius= "50%";
   mouth.style.width= "26vh";
   left_eyelid.style.height = "0%";
   right_eyelid.style.height = "0%";
   left_eye.style.height = "100%";
   right_eye.style.height = "100%";
+  upper_lip.style.height = "0%";
 })
 
 
@@ -76,5 +80,37 @@ smile.addEventListener("click", ()=>{
     right_eyelid.style.height = "85%";
     left_eye.style.height = "80%";
     right_eye.style.height = "80%";
+    upper_lip.style.height = "0%";
 })
 
+happy.addEventListener("click", ()=>{
+  mouth.style.borderTopRightRadius= "5px";
+    mouth.style.borderTopLeftRadius= "5px";
+    mouth.style.borderBottomLeftRadius= "50%";
+    mouth.style.borderBottomRightRadius= "50%";
+    mouth.style.width= "80%";
+    left_eyelid.style.height = "80%";
+    right_eyelid.style.height = "80%";
+    left_eye.style.height = "90%";
+    right_eye.style.height = "90%";
+    upper_lip.style.height = "80%";
+})
+
+
+wink.addEventListener("click", ()=>{
+  mouth.style.borderTopRightRadius= "5px";
+    mouth.style.borderTopLeftRadius= "5px";
+    mouth.style.borderBottomLeftRadius= "50%";
+    mouth.style.borderBottomRightRadius= "50%";
+    mouth.style.width= "80%";
+    left_eyelid.style.height = "0%";
+    right_eyelid.style.height = "0%";
+    left_eye.style.height = "90%";
+    right_eye.style.height = "90%";
+    upper_lip.style.height = "0%";
+    setTimeout(() => {
+      left_eye.style.height = "100%";
+      right_eye.style.height = "100%"; 
+    }, 200);
+
+})
